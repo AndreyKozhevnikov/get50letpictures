c5fe36ff67a17666e5a4a68811e5ba5df67a7507
Andrey <andrey.kozhevnikov@devexpress.com>
2021-04-05 22:23:11 +0300

    upload to web

---
+++ downloadFiles.js
@@ -0,0 +1,25 @@
+let ImageItem = require('./models/ImageItem.js');
+let fs = require('fs');
+let mongoose = require('mongoose');
+let mongoDB ='mongodb://localhost:27017/getImageTest';
+mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true  });
+
+async function getImages(){
+  let item_list = await ImageItem.find();
+  item_list.forEach(x=>{
+    fs.writeFile('./Images/'+x.Name+'.jpg',x.Data,(err)=>{
+
+    });
+  });
+}
+getImages();
+console.log('done!');
+function formatDate(date) {
+  let d = new Date(date);
+  let month = '' + (d.getMonth() + 1);
+  let day = '' + d.getDate();
+  let year = d.getFullYear();
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  return [year, month, day].join('-');
+}
--- test.js
+++ test.js
@@ -1,34 +1,43 @@
 let ImageItem = require('./models/ImageItem.js');
-var fs = require('fs'),
-   request = require('request');
+var request = require('request');
 
-var download = function(uri, filename, callback){
+var download = function(uri ){
   request.head(uri, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+   request({url:uri, encoding:null}, function (error, response, body) {
+   
+      let item = new ImageItem();
+      item.Name='Image2-'+formatDate(new Date());
+      item.Data =body;
+      item.save((err)=>{});
+      console.log('done2');
+        // handle errors etc.
+    }); 
+
    
   });
 };
 let mongoose = require('mongoose');
-let mongoDB ='mongodb://localhost:27017/getImageTest';
-mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true  });
-
-download('http://194.190.129.43/ships/foto/50letpobedy1.jpg', 'google3.png', function(){
-  let item = new ImageItem();
-  item.Name='Image-'+formatDate(new Date());
-  item.Data =fs.readFileSync('google3.png');
-  item.save();
-  console.log('done');
+//let mongoDB ='mongodb://localhost:27017/getImageTest';
+//let mongoDB ='mongodb+srv://testName:<testPass>@cluster0.mergb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//let mongoDB ='mongodb+srv://testName:<testPass>@cluster0.mergb.mongodb.net/test';
+let mongoDB ='mongodb+srv://testName:testPass@cluster0.mergb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true  },(err)=>{
+  console.log(err);
 });
+
+download('http://194.190.129.43/ships/foto/50letpobedy1.jpg');
 
 function formatDate(date) {
   let d = new Date(date);
   let month = '' + (d.getMonth() + 1);
   let day = '' + d.getDate();
   let year = d.getFullYear();
+  let hour = d.getHours();
+  let mins = d.getUTCMilliseconds();
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
-  return [year, month, day].join('-');
+  return [year, month, day,hour,mins].join('-');
 }

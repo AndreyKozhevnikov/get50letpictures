e0e9219f06046162092fa8d042807028fceab90f
Andrey <andrey.kozhevnikov@devexpress.com>
2021-04-06 15:18:31 +0300

    set IsDownloaded

--- downloadFiles.js
+++ downloadFiles.js
@@ -6,21 +6,15 @@ let mongoDB ='mongodb+srv://testName:testPass@cluster0.mergb.mongodb.net/myFirst
 mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true  });
 
 async function getImages(){
-  let item_list = await ImageItem.find();
+  let item_list = await ImageItem.find({IsDownloaded:{ $exists: false }});
   item_list.forEach(x=>{
     fs.writeFile('./Images/'+x.Name+'.jpg',x.Data,(err)=>{
-
+console.log('2' + err);
     });
+    x.IsDownloaded=true;
+    x.save((err)=>{console.log('1' + err);});
+    console.log('done' + x.Name);
   });
 }
 getImages();
 console.log('done!');
-function formatDate(date) {
-  let d = new Date(date);
-  let month = '' + (d.getMonth() + 1);
-  let day = '' + d.getDate();
-  let year = d.getFullYear();
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-  return [year, month, day].join('-');
-}
--- models/ImageItem.js
+++ models/ImageItem.js
@@ -6,6 +6,7 @@ let Schema = mongoose.Schema;
 let ImageItemSchema = new Schema({
   Name: {type: String, required: true, max: 100},
   Data: {type: Buffer},
+  IsDownloaded: {type:Boolean},
 });
 
 module.exports = mongoose.model('ImageItem', ImageItemSchema);
